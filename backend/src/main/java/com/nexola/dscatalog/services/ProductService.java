486aac52b2d28dcba877cd61f50d9b304b9be015
nexola <vitormatheusfv@gmail.com>
2024-01-24 18:42:24 -0300

    ProductRepository delete test

--- backend/src/main/java/com/nexola/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/nexola/dscatalog/services/ProductService.java
@@ -62,9 +62,6 @@ public class ProductService {
 
     @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
-        if (!repository.existsById(id)) {
-            throw new ResourceNotFoundException("Recurso não encontrado");
-        }
         try {
             repository.deleteById(id);
         }
---
+++ backend/src/test/java/com/nexola/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,25 @@
+package com.nexola.dscatalog.repositories;
+
+import com.nexola.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+        long existingId = 1L;
+        repository.deleteById(existingId);
+
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+}
