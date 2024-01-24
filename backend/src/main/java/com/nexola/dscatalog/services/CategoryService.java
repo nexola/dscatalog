71f8d746820b2408c87cde3fd0b683e5dfeaeb5a
nexola <vitormatheusfv@gmail.com>
2024-01-23 22:19:44 -0300

    Insert Category

--- backend/src/main/java/com/nexola/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/nexola/dscatalog/services/CategoryService.java
@@ -31,4 +31,13 @@ public class CategoryService {
         return new CategoryDTO(cat);
     }
 
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
+
+
 }
