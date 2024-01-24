620a1bfce90ee8e019d74fb5e9ef97314eaff78a
nexola <vitormatheusfv@gmail.com>
2024-01-23 21:51:15 -0300

    Seeding

--- backend/src/main/java/com/nexola/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/nexola/dscatalog/services/CategoryService.java
@@ -5,6 +5,7 @@ import com.nexola.dscatalog.entities.Category;
 import com.nexola.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +16,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
         return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
