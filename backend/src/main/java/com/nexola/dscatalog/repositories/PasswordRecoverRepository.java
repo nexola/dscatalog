092b42a70eee56214b3b22fe299777300ee1d25b
nexola <vitormatheusfv@gmail.com>
2024-02-02 13:08:10 -0300

    Email de recuperação de senha

---
+++ backend/src/main/java/com/nexola/dscatalog/repositories/PasswordRecoverRepository.java
@@ -0,0 +1,9 @@
+package com.nexola.dscatalog.repositories;
+
+import com.nexola.dscatalog.entities.PasswordRecover;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordRecoverRepository extends JpaRepository<PasswordRecover, Long> {
+}
--- backend/src/main/java/com/nexola/dscatalog/services/AuthService.java
+++ backend/src/main/java/com/nexola/dscatalog/services/AuthService.java
@@ -1,12 +1,52 @@
 package com.nexola.dscatalog.services;
 
 import com.nexola.dscatalog.dto.EmailDTO;
+import com.nexola.dscatalog.entities.PasswordRecover;
+import com.nexola.dscatalog.entities.User;
+import com.nexola.dscatalog.repositories.PasswordRecoverRepository;
+import com.nexola.dscatalog.repositories.UserRepository;
+import com.nexola.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class AuthService {
 
-    public void createRecoverToken(EmailDTO body) {
+    @Value("${email.password-recover.token.minutes}")
+    private Long tokenMinutes;
 
+    @Value("${email.password-recover.uri}")
+    private String recoverURI;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordRecoverRepository passwordRecoverRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    public void createRecoverToken(EmailDTO body) {
+        User user = repository.findByEmail(body.getEmail());
+        if (user == null) {
+            throw new ResourceNotFoundException("Email não encontrado");
+        }
+
+        PasswordRecover entity = new PasswordRecover();
+        entity.setEmail(body.getEmail());
+        entity.setToken(UUID.randomUUID().toString());
+        entity.setExpiration(Instant.now().plusSeconds(tokenMinutes * 60L));
+
+        entity = passwordRecoverRepository.save(entity);
+
+        String bodyEmail = "Acesse o link para definir uma nova senha\n\n"
+                + recoverURI + entity.getToken() + "\n\nVálido por " + tokenMinutes + " minutos";
+
+        emailService.sendEmail(entity.getEmail(), "Recuperação de senha", bodyEmail);
     }
 }
